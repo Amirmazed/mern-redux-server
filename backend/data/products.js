778a9f2dc3911113a8fa8264ef48562389628b43
Amirmazed <76693565+Amirmazed@users.noreply.github.com>
2021-12-20 12:53:36 +0600

    first commit

---
+++ backend/data/products.js
@@ -0,0 +1,68 @@
+
+const products = [
+  {
+    name: "Rolex Men's Metallic X 777 ",
+    imageUrl:
+      "https://i.ibb.co/8BsskGf/637729158440799621.jpg",
+    description:
+    "This silver tone 777 Rolex GMT Master Batman watch is made from stainless steel with a bracelet strap. It features a black and blue rotating bezel, a 40 mm case, sapphire glass lens, black dial, white hands and indices, a cyclops date window and original Rolex branding. As its name suggests, this customised timepiece is the Batman to your Robin, for everyone knows there's no finer sidekick than a 777 watch.",
+    price: 499,
+    countInStock: 15,
+  },
+  {
+    name: "ROLEX Oyster Perpetual Datejust 41",
+    imageUrl:
+      "https://i.ibb.co/NygZfQV/H0b9d7899f1434a01a6158e7df075bb4dz-jpg-220x220.jpg",
+    description:
+      "Rolex Oyster Perpetual. Datejust. Oyster-steel bracelet material with flat three-piece links. Date aperture. Comes in a signature green case with crown logo plaque. Swiss-made. 41 mm case with black analog dial. Oyster steel. Stainless steel domes bezel. Superlative chronometer. Hour, minute and seconds hands. Oyster bracelet fitted with Oyster folding clasp. Monobloc middle case, screw-down case back and winding crown. 41 mm. Oyster steel. Scratch-resistant sapphire, Cyclops lens over the date. Waterproof to 100 meters / 330 feet. Color: black",
+    price: 1099,
+    countInStock: 10,
+  },
+  {
+    name: "ROLEX Vintage Rolex Stainless Steel",
+    imageUrl:
+      "https://i.ibb.co/3mDVpFj/rolex-black-Estate-Rolex-Stainless-Steel-Datejust-Watch-36mm.jpg",
+    description:
+      "Men's Metallic Vintage Rolex Stainless Steel Datejust Watch, 36mm We checked over 450 stores and this item is limited",
+    price: 1300,
+    countInStock: 5,
+  },
+  {
+    name: "ROLEX Estate Rolex",
+    imageUrl:
+      "https://i.ibb.co/FYqqbCM/rolex-Black-Gold-Tone-Pink-Rose-Gold-T-Yacht-master-Diamond-Pave-Dial-Mens-Watch-126655-0005.jpg",
+    description:
+      "Pre-Owned Rolex Daytona Cosmograph (116508) self-winding automatic watch, features a 40mm 18k yellow gold case surrounding a black dial on an 18k yellow gold Oyster bracelet with folding buckle. Functions include hours, minutes, small seconds, chronograph and tachymeter. This watch does not come with box or papers. Scattered Serial. We back this watch with a 2-Yeat WatchBox warranty",
+    price: 50,
+    countInStock: 25,
+  },
+  {
+    name: "ROLEX Datejust Sundust",
+    imageUrl:
+      "https://i.ibb.co/YRqR6NS/rolex-Black-Oyster-Perpetual-Datejust-41-Dial-Onesize.jpg",
+    description:
+    "Pre-Owned Rolex Daytona Cosmograph (116509) self-winding automatic watch, features a 40mm 18k white gold case surrounding a black dial on an 18k white gold Oyster bracelet with folding buckle. Functions include hours, minutes, small seconds, chronograph and tachymeter.",
+    price: 233,
+    countInStock: 4,
+  },
+  {
+    name: "ROLEX Yacht-master Diamond",
+    imageUrl:
+      "https://i.ibb.co/f8RkVtd/rolex-Metallic-X-777-Gmt-Master-Ii-Batman-Watch.jpg",
+    description:
+      "ROLEX Yacht-master Diamond is the next generation  on an 18k yellow gold Oyster bracelet with folding buckle. Functions include hours, minutes, small seconds, chronograph and tachymeter.",
+    price: 140,
+    countInStock: 10,
+  },
+  {
+    name: "ROLEX Day Date 40",
+    imageUrl:
+      "https://i.ibb.co/Mf9XPXN/rolex-two-tone-Vintage-Rolex-Stainless-Steel-Datejust-Watch-36mm.jpg",
+    description:
+      "ROLEX Day Date 40 Wood Watches For Men Quartz Wrist Band Stopwatch Male Auto Dial Display Auto Date Wristwatches",
+    price: 140,
+    countInStock: 10,
+  },
+];
+
+module.exports = products;
